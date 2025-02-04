59e5167ad391531063f54bbd24478ecdb9770df7
gr00ze <vincenzop23@gmail.com>
2025-02-05 00:21:08 +0100

    1. Reorganized the code to a more readable version 2. Removed deprecated method 3. Added group chat support

--- src/main/java/tutorial/BotLogic.java
+++ src/main/java/tutorial/BotLogic.java
@@ -4,6 +4,7 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -11,26 +12,65 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.util.List;
 
 public class BotLogic extends TelegramLongPollingBot {
+
+    public BotLogic(String botToken){
+        super(botToken);
+    }
+
+
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage();            // Create a SendMessage object with mandatory fields
-            message.setChatId(update.getMessage().getChatId().toString());
-            message.setText("This bot handles only images.");   //  Custom text upon receiving a text message
+        if (update.hasMessage())
+            onMessageReceived(update.getMessage());
+
+
+
+    }
+
+
+    private void onMessageReceived(Message message){
+        //Should be private chat
+        if(message.isUserMessage()){
+            userMessageBehaviour(message);
+
+        }else if(message.isGroupMessage()){
+            //new behaviour to develop
+            groupMessageBehaviour(message);
+        }
+
+    }
+    private void userMessageBehaviour(Message message) {
+        if (message.hasPhoto()) {
+            // If the message received is a "photo" type, it'll echo it but blurred
+            this.sendPhotoWithSpoiler(message.getChatId(),message.getPhoto());
+        }else if (message.hasText()) {
+            SendMessage sendMessage = new SendMessage();            // Create a SendMessage object with mandatory fields
+            sendMessage.setChatId(message.getChatId().toString());
+            sendMessage.setText("This bot handles only images.");   //  Custom text upon receiving a text message
             try {
-                execute(message);                               // Call method to send the message
+
+                execute(sendMessage);                               // Call method to send the message
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
-        } else if (update.hasMessage() && update.getMessage().hasPhoto()) {
-            // If the message received is a "photo" type, it'll echo it but blurred
-            List<PhotoSize> photos = update.getMessage().getPhoto();
-            // Takes only the largest photo for best quality (usually the last)
-            String fileId = photos.get(photos.size() - 1).getFileId();
-            sendPhotoWithSpoiler(update.getMessage().getChatId(), fileId);
         }
     }
-    private void sendPhotoWithSpoiler(Long chatId, String photoFileId) {
+
+
+    private void groupMessageBehaviour(Message message) {
+        //if u tag it on group chat
+        if(message.hasText() && message.getText().contains(this.getBotUsername()) && message.isReply()){
+            Message replyToMessage = message.getReplyToMessage();
+            if (replyToMessage.hasPhoto())
+                this.sendPhotoWithSpoiler(message.getChatId(),message.getPhoto());
+        }
+
+    }
+
+    private void sendPhotoWithSpoiler(Long chatId, List<PhotoSize> photos) {
+        // Takes only the largest photo for best quality (usually the last)
+        String photoFileId = photos.get(photos.size() - 1).getFileId();
+
         SendPhoto sendPhoto = new SendPhoto();
         sendPhoto.setChatId(chatId);                            //  Syncs it with the Chat's ID
         sendPhoto.setPhoto(new InputFile(photoFileId));         //  Creates the new "photo" file to be sent
@@ -42,21 +82,16 @@ public class BotLogic extends TelegramLongPollingBot {
             e.printStackTrace();
         }
     }
+
+
     @Override
     public String getBotUsername() {
-        return "Your_Bot_Username_Here";
+        try {
+            return getMe().getUserName();
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
     }
-    @Override
-    public String getBotToken() {
-        return "Your_Bot_Token_Here";
-    }
-    //  I'm not sure if the last two here are necessary
-    @Override
-    public void onRegister() {
-        super.onRegister();
-    }
-    @Override
-    public void onUpdatesReceived(List<Update> updates) {
-        super.onUpdatesReceived(updates);
-    }
+
 }
--- src/main/java/tutorial/Main.java
+++ src/main/java/tutorial/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new BotLogic());    //  Updates the bot and puts the functionality online
+            botsApi.registerBot(new BotLogic("Your_Bot_Token_Here"));    //  Updates the bot and puts the functionality online
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
