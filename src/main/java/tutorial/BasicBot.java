be1ff4127016aa44d55d66387e5b95d76cf12d37
gr00ze <vincenzop23@gmail.com>
2025-02-06 00:59:53 +0100

    Removed unused imports

--- src/main/java/tutorial/BasicBot.java
+++ src/main/java/tutorial/BasicBot.java
@@ -1,14 +1,9 @@
 package tutorial;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
-import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-import java.util.List;
 
 public class BasicBot extends TelegramLongPollingBot {
 
